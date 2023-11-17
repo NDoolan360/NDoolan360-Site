6c8d9d766b9454067f507affda94927e4c0c2d32
Nathan Doolan <nathan.doolan@gentrack.com>
2023-11-18 10:58:30 +1100

    Fix failing theme and copyright tests

--- src/copyright.ts
+++ src/copyright.ts
@@ -1,3 +1,8 @@
-// Update copyright year
-const copyright = document.getElementById('copyright')!;
-copyright.innerHTML = copyright?.innerHTML.replace('{current year}', new Date().getFullYear().toString());
+export const replaceWithCurrentYear = (input: string, match: string): string => {
+    return input.replace(match, new Date().getFullYear().toString());
+};
+
+const copyright = document.getElementById('copyright');
+if (copyright) {
+    copyright.innerHTML = replaceWithCurrentYear(copyright.innerHTML, '{current year}');
+}
