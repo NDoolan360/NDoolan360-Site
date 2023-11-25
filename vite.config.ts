55066dbd122a89f0c54f279428bd45451bf3707c
nathan.doolan <nathan.doolan@gentrack.com.au>
2023-11-25 19:28:29 +1100

    Remove eslint, prettier and stylelint

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite';
-import eslint from 'vite-plugin-eslint';
 import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
-    plugins: [eslint()],
     css: {
         postcss: {
             plugins: [autoprefixer()],
