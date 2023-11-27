799f5ef7816d6142df5e5da93f633bd1e531cca3
nathan.doolan <nathan.doolan@gentrack.com.au>
2023-11-27 13:55:39 +1100

    Move theme button to loaded template

--- src/theme.ts
+++ src/theme.ts
@@ -38,12 +38,16 @@ window.onload = () => {
     // set on load so screen readers can see latest value on the button
     reflectPreference(theme);
 
-    // Button element adapted from:
-    // https://web.dev/patterns/theming/theme-switch/#html
-    const themeSwitch = document.getElementById("theme-toggle");
-    if (themeSwitch) {
-        themeSwitch.addEventListener("click", onClick);
-        themeSwitch.innerHTML = `<svg class="sun-and-moon" aria-hidden="true" width="24" height="24" viewBox="0 0 24 24"><mask class="moon" id="moon-mask"><rect x="0" y="0" width="100%" height="100%" fill="white" /><circle cx="24" cy="10" r="6" fill="black" /></mask><circle class="sun" cx="12" cy="12" r="6" mask="url(#moon-mask)" fill="currentColor" /><g class="sun-beams" stroke="currentColor"><line x1="12" y1="1" x2="12" y2="3" /><line x1="12" y1="21" x2="12" y2="23" /><line x1="4.22" y1="4.22" x2="5.64" y2="5.64" /><line x1="18.36" y1="18.36" x2="19.78" y2="19.78" /><line x1="1" y1="12" x2="3" y2="12" /><line x1="21" y1="12" x2="23" y2="12" /><line x1="4.22" y1="19.78" x2="5.64" y2="18.36" /><line x1="18.36" y1="5.64" x2="19.78" y2="4.22" /></g></svg>`;
+    const themeTemplate = document.getElementById("theme-toggle-template") as
+        | HTMLTemplateElement
+        | undefined;
+    if (themeTemplate) {
+        const themeTemplateClone = document.importNode(themeTemplate.content, true);
+        themeTemplate.parentElement?.appendChild(themeTemplateClone);
+    }
+    const themeToggle = document.getElementById("theme-toggle");
+    if (themeToggle) {
+        themeToggle.addEventListener("click", onClick);
     }
 };
 
